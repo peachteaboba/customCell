0509b23b60c7a01a779b6b971c30d45e1cc76572
Andy Feng <anndyfeng1@gmail.com>
2016-08-08 08:38:54 -0700

    first commit

---
+++ CustomCell/CustomCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCell.swift
+//  CustomCell
+//
+//  Created by Andy Feng on 8/8/16.
+//  Copyright © 2016 Andy Feng. All rights reserved.
+//
+
+import UIKit
+class CustomCell: UITableViewCell {
+    
+
+    
+    
+}
